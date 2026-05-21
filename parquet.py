95396a168de0514eba2c42bc37d0460702b4011a
Jususs <j.warm@gmx.at>
2026-05-21 15:26:55 +0200

    Added weather parquet and its partitions

--- parquet.py
+++ parquet.py
@@ -5,19 +5,21 @@ import pyarrow.parquet as pq
 import pyarrow.csv as csv
 import pandas as pd
 
-# table = csv.read_csv("csv/Automated_Traffic_Volume_Counts_20260520.csv")
-# pq.write_table(table, "traffic_vol_current.parquet")
+# table = csv.read_csv("csv/nyc_weather.csv")
+# pq.write_table(table, "weather.parquet")
 
-myTable = pq.read_table("vehicle_collisions.parquet")
+# myTable = pq.read_table("vehicle_collisions.parquet")
 # myTable = pq.read_table("traffic_vol_hist.parquet")
 # myTable = pq.read_table("traffic_vol_current.parquet")
 # myTable = pq.read_table("permitted_event_hist.parquet")
+myTable = pq.read_table("weather.parquet")
 # print(myTable)
 
 df = myTable.to_pandas() # to handle date fields better
-df['parsed_date'] = pd.to_datetime(df['CRASH DATE'], format='%m/%d/%Y' ,errors='coerce')
+# df['parsed_date'] = pd.to_datetime(df['CRASH DATE'], format='%m/%d/%Y' ,errors='coerce')
 # df['parsed_date'] = pd.to_datetime(df['Date'], format='%m/%d/%Y', errors='coerce')
 # df['parsed_date'] = pd.to_datetime(df['Start Date/Time'], format='%m/%d/%Y %I:%M:%S %p', errors='coerce')
+df['parsed_date'] = pd.to_datetime(df['DATE'], format='%Y-%m-%dT%H:%M:%S', errors='coerce')
 df['year'] = df['parsed_date'].dt.year.astype('Int64').astype(str)
 df['month'] = df['parsed_date'].dt.month.astype('Int64').astype(str)
 
@@ -25,10 +27,11 @@ df = df.drop(columns=['parsed_date']) #optional
 
 myTable = pa.Table.from_pandas(df)
 
-root_path="vehicle_collisions_partitioned"
+# root_path="vehicle_collisions_partitioned"
 # root_path="traffic_vol_hist_partitioned"
 # root_path="traffic_vol_current_partitioned"
 # root_path="permitted_event_hist_partitioned"
+root_path="weather_partitioned"
 metadata_collector = []
 
 if os.path.exists(root_path):
